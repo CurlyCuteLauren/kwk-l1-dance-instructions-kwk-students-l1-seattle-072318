3efe906820166b862eff6f352db98da84dc50202
CurlyCuteLauren <lnziegele@gmail.com>
2018-08-01 21:59:30 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,22 @@
-
+def Gangnam_Style
+puts "Assume_the_proper_stance"
+puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
+puts "Base Footwork"
+puts "1. Lift right foot"
+puts "2. Return right"
+puts "3. Finishing with a small skip-step backward"
+puts "Skip Step Left"
+puts "1. Lower left foot to the ground"
+puts "2. Bounce left foot back up slightly, kicking it a few inches back"
+puts "3. Left, Right, Left and Left"
+puts "Skip Step Right"
+puts "1. Lower right foot to the ground"
+puts "Upper Body"
+puts "Bob The Reins"
+puts "1. Holding your arms out in front of you, straight and at chest level"
+puts "2. Cross your right wrist over your left and hold them together"
+puts "3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+puts "Lasso"
+puts "1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+puts ""
 # Code your methods below
